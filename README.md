256ff4e0f3320b0794f43b1fdc12a7992a3f4f93
Ryan Burge <ryanburge@gmail.com>
2017-05-05 22:03:12 -0500

    I figured out to dplyr better.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Slot_Machine
 Slot Machine Revenue Collected by Illinois Towns and Cities in March 2017
+
+![alt text](https://raw.githubusercontent.com/ryanburge/Slot_Machine/master/screenshot.png)
+
+![alt text](https://raw.githubusercontent.com/ryanburge/Slot_Machine/master/march_losses.png)
+
--- march_est.R
+++ march_est.R
@@ -7,14 +7,12 @@ library(fuzzyjoin)
 library(readr)
 library(ggmap)
 
+## January
+slots <- read_csv("est_jan.csv", skip =3)
 
-slots <- read_csv("est_march.csv", skip =3)
-
-slots$fundsin <- abs(slots$`Funds In`)
-slots$fundsout <- abs(slots$`Funds Out`)
-
-slots$profit <- slots$fundsin - slots$fundsout
-
+slots <- slots %>% filter(`VGT Count` > 0) %>% 
+  rename(fundsin = `Funds In`, fundsout = `Funds Out`) %>%
+  mutate(profit = fundsin - fundsout)
 
 
 t1 <- slots %>% select(Establishment, Municipality, fundsin, fundsout, profit) 
@@ -22,4 +20,35 @@ t1 <- slots %>% select(Establishment, Municipality, fundsin, fundsout, profit)
 t1$est <- paste(t1$Establishment, t1$Municipality, sep = ", ")
 
 ggplot(t1 %>% filter(profit <0), aes(x=reorder(est, -profit), y=profit)) + geom_col() + coord_flip()
+
+
+## February
+slots <- read_csv("est_feb.csv", skip =3)
+
+slots <- slots %>% filter(`VGT Count` > 0) %>% 
+  rename(fundsin = `Funds In`, fundsout = `Funds Out`) %>%
+  mutate(profit = fundsin - fundsout)
+
+
+t1 <- slots %>% select(Establishment, Municipality, fundsin, fundsout, profit) 
+
+t1$est <- paste(t1$Establishment, t1$Municipality, sep = ", ")
+
+ggplot(t1 %>% filter(profit <0), aes(x=reorder(est, -profit), y=profit)) + geom_col() + coord_flip()
+
+
+## March 
+slots <- read_csv("est_march.csv", skip =3)
+
+slots <- slots %>% filter(`VGT Count` > 0) %>% 
+  rename(fundsin = `Funds In`, fundsout = `Funds Out`) %>%
+  mutate(profit = fundsin - fundsout)
+
+
+t1 <- slots %>% select(Establishment, Municipality, fundsin, fundsout, profit) 
+
+t1$est <- paste(t1$Establishment, t1$Municipality, sep = ", ")
+
+ggplot(t1 %>% filter(profit <0), aes(x=reorder(est, -profit), y=profit)) + geom_col() + coord_flip()
+
 
